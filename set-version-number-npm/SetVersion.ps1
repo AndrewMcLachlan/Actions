c9f6fef7a81c7c42b5b22731b632f083e912de4d
Andrew McLachlan <andy@mclachlan.family>
2023-08-19 17:17:16 +1000

    Updated set NPM version to allow for suffixes.

--- set-version-number-npm/SetVersion.ps1
+++ set-version-number-npm/SetVersion.ps1
@@ -13,16 +13,22 @@ if ($null -eq $res) {
     throw "Could not find version in file '$PackageFilePath'"
 }
 
+Write-Output "Read version $res from package.json"
+
 $version = $null
-$parsed = [System.Version]::TryParse($res, [ref]$version);
+$split = $res.split("-")
+$suffix = ($split.length -gt 1) ? "-$($split[1])" : ""
+
+$parsed = [System.Version]::TryParse($split[0], [ref]$version);
 
 if (!$parsed) {
     throw "Could not parse version '$res' in file '$PackageFilePath'"
 }
 
 $newVersion = [System.Version]::new($version.Major, $version.Minor, $RunNumber)
+$newVersionComplete = "$($newVersion.ToString())$suffix";
 
-Write-Output "Version is $($newVersion.ToString())"
-Write-Output "version=$($newVersion.ToString())" >> $Env:GITHUB_OUTPUT
+Write-Output "Version is $newVersionComplete"
+Write-Output "version=$newVersionComplete" >> $Env:GITHUB_OUTPUT
 Write-Output "Output file is $Env:GITHUB_OUTPUT"
 Get-Content $Env:GITHUB_OUTPUT | Write-Output
