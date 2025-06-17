be5892193bae0639de7ae28a01860d6d5703a0cc
Andrew McLachlan <andy@mclachlan.family>
2025-06-17 21:46:21 +1000

    Added an action that just gets the major and minor version from an MSBuild file.

---
+++ get-version-number-from-project/GetVersion.ps1
@@ -0,0 +1,21 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$Project,
+    [Parameter(Mandatory = $true)]
+    [string]$VersionPropertyName
+)
+
+$res = dotnet msbuild -getProperty:$VersionPropertyName $Project
+
+$version = $null
+$parsed = [System.Version]::TryParse($res, [ref]$version);
+
+if (!$parsed) {
+    throw "Could not parse version '$($res)' in project '$Project'"
+}
+
+Write-Output "Version is $($newVersion.ToString())"
+Write-Output "major=$($newVersion.Major)" >> $Env:GITHUB_OUTPUT
+Write-Output "minor=$($newVersion.Minor)" >> $Env:GITHUB_OUTPUT
+Write-Output "Output file is $Env:GITHUB_OUTPUT"
+Get-Content $Env:GITHUB_OUTPUT | Write-Output
