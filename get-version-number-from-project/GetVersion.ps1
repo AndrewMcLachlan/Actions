4af5dd5faf28fae96ee76c9c19caed19cc4ae392
Andrew McLachlan <andy@mclachlan.family>
2025-06-21 11:20:28 +1000

    newVersion now version.

--- get-version-number-from-project/GetVersion.ps1
+++ get-version-number-from-project/GetVersion.ps1
@@ -14,8 +14,8 @@ if (!$parsed) {
     throw "Could not parse version '$($res)' in project '$Project'"
 }
 
-Write-Output "Version is $($newVersion.ToString())"
-Write-Output "major=$($newVersion.Major)" >> $Env:GITHUB_OUTPUT
-Write-Output "minor=$($newVersion.Minor)" >> $Env:GITHUB_OUTPUT
+Write-Output "Version is $($version.ToString())"
+Write-Output "major=$($version.Major)" >> $Env:GITHUB_OUTPUT
+Write-Output "minor=$($version.Minor)" >> $Env:GITHUB_OUTPUT
 Write-Output "Output file is $Env:GITHUB_OUTPUT"
 Get-Content $Env:GITHUB_OUTPUT | Write-Output
